2fc7e154a1c04cfd0c5834e82e5c0cce5e2a22d6
GabsMN <gabrielmoura0282@gmail.com>
2025-02-11 10:56:52 -0300

    Update Principal.java

--- vagasExpress/src/main/java/interfaceDeUso/Principal.java
+++ vagasExpress/src/main/java/interfaceDeUso/Principal.java
@@ -1,8 +1,12 @@
 package corpoPrograma;
 
+import java.awt.CardLayout;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 
 public class Principal {
 
@@ -11,8 +15,11 @@ public class Principal {
     private String IDFiltro;
     private String IDRemove;
     private String IDEdit;
+    private static final String senha = "123456";
+    private JPanel painelPrincipal;
 
-    public Principal() {
+    public Principal(JPanel painel) {
+        this.painelPrincipal = painel;
         IDPrinc = "";
         IDFiltro = "";
         IDRemove = "";
@@ -27,6 +34,24 @@ public class Principal {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void verificaSenha() {
+        JPasswordField tentativa = new JPasswordField();
+
+        int resposta = JOptionPane.showConfirmDialog(null, tentativa, "Digite a senha de administrador", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        if (resposta == JOptionPane.OK_OPTION) {
+            String senhaDig = new String(tentativa.getPassword());
+
+            if (senhaDig.equals(senha)) { // Se a senha for correta, troca o Card para o de Adm
+                CardLayout troca = (CardLayout) painelPrincipal.getLayout();
+                troca.show(painelPrincipal, "card4");
+            } else {
+                JOptionPane.showMessageDialog(null, "Senha incorreta!", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
     }
 
 }
